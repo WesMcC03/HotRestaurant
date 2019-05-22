69c8b936df4381a16c15e1e06f4182b5aab169c3
Armando Morales <amorales944@gmail.com>
2019-05-21 23:24:20 -0500

    Fixed file name endings

--- app/public/server.js
+++ app/public/server.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
-
-
+// const server = http.createServer(handleRequest);
+// function handleRequest(req, res) {
+//     var path = req.url;
+//     switch (path) {
+//         case "/thanks":
+//           return renderThankYouPage(req, res);
+//         default:
+//           return renderWelcomePage(req, res);
+//         }
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/home", function(req, res) {
+app.get("/home.html", function(req, res) {
     // res.send("Welcome to Hot Restaurant home page")
     res.sendFile(path.join(__dirname, "./home.html"));
   });
   
-  app.get("/reservations", function(req, res) {
+app.get("/makesreservations.html", function(req, res) {
     // res.send("Welcome to Hot Restaurant make reservations page")
     res.sendFile(path.join(__dirname, "./makereservations.html"));
   });
 
-  app.get("/viewtables", function(req, res) {
+app.get("/viewtables.html", function(req, res) {
     // res.send("Welcome to Hot Restaurant view tables page")
     res.sendFile(path.join(__dirname, "./viewtables.html"));
   });
